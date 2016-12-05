61479d975ada1f69898b2345f36bcd32f34405ed
cdokkum <Cor@MacBook-Pro-van-Cor.local>
2016-12-05 21:22:11 +0100

    Bestanden toegevoegd aan algemeen.

---
+++ Html pagina's/Algemeen/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>Autoquest - Login</title>
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+        <link href="../bootstrap-files/ie10-viewport-bug-workaround.css" rel="stylesheet">
+        <!-- Custom styles for this template -->
+        <link href="css/login.css" rel="stylesheet">
+    </head>
+    <body>
+        <!-- navigation bar -->
+        <?php include 'navbar.php';?>
+    </body>
+</html>
