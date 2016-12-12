a54edc7c22038abd46a31f79e8f9bb043f0cbb12
cdokkum <Cor@84-132.dhcp.windesheim.nl>
2016-12-12 11:13:50 +0100

    Map Algemeen bestanden toegevoegd.

---
+++ Html pagina's/Algemeen/include/cpanelnavbar.php
@@ -0,0 +1,13 @@
+<?php
+
+//cpanel navbar
+print "<div class='cpanel-navbar'>"
+        . "<ul class='nav nav-pills'>"
+        . "<li role='presentation'><a href='cpaneloverview.php'>Overview</a></li>"
+        . "<li role='presentation'><a href='cpanelklanten.php'>klanten</a></li>"
+        . "<li role='presentation'><a href='cpanelproducten.php'>Producten</a></li>"
+        . "<li role='presentation'><a href='logout.php'>logout</a></li>"
+        . "</ul>"
+        . "</div>";
+?>
+
